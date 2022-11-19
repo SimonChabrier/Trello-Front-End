1a0c94d200de64a2f51eddb9de90e058b98ad167
Simon Chabrier <simonchabrier@gmail.com>
2022-11-19 22:44:22 +0100

    change colors and add task done

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,7 @@ trelloListeners:()=> {
       app.handleDeleteCard();
       app.countNewCard();
       app.changeCardColor();
+      app.handleTaskDone();
     });
 
   document.getElementById('fullscreen_switch').addEventListener('change', (event) => {
@@ -47,7 +48,7 @@ handleDeleteCard:() => {
 
 handleCreateColumn:() => {
   const column = app.createElement('div', 'cards_dropzone', '');
-  column.appendChild(app.createInputElement('input', 'input--column--name', 'Column name'));
+  column.appendChild(app.createInputElement('input','input', 'input--column--name', 'input--column--name', 'todo'));
   app.appendElementToSelector(column,'.columns--container');
   const btn = app.createElement('button', 'delete_column', 'X');
   column.appendChild(btn);
@@ -62,10 +63,36 @@ handleCreateCard:() => {
   app.appendElementToSelector(card,'.new--card--section');
 },
 
+handleTaskDone:() => {
+  const checkboxes = document.querySelectorAll('.card--checkox');
+  
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', (event) => {
+      if (event.target.checked) {
+        event.target.closest('div').classList.add('task--done');
+        const inputs = event.target.closest('section').querySelectorAll('.card--text, .card--title');
+        inputs.forEach(input => {
+          input.setAttribute('disabled', true);
+        });
+
+      } else {
+        event.target.closest('div').classList.remove('task--done');
+        const inputs = event.target.closest('section').querySelectorAll('.card--text, .card--title');
+        inputs.forEach(input => {
+          input.removeAttribute('disabled', true);
+        });
+
+      }
+    });
+  });
+
+},
+
 setCardContent:() => {
-  const cardContent = app.createElement('section', 'card_content', null); 
-  cardContent.appendChild(app.createInputElement('input', 'card--title', 'Title'));
-  cardContent.appendChild(app.createInputElement('textarea', 'card--text', 'Description'));
+  const cardContent = app.createElement('section', 'card_content', ''); 
+  cardContent.appendChild(app.createInputElement('input', 'text', 'task_title',  'card--title', 'Title'));
+  cardContent.appendChild(app.createInputElement('textarea', '', 'task_content', 'card--text', 'Description'));
+  cardContent.appendChild(app.createInputElement('input', 'checkbox', 'task_status', 'card--checkox', ''));
   
   return cardContent;
 },
@@ -74,14 +101,14 @@ headerCardColors:() => {
   section = app.createElement('section', 'card--colors', null);
   section.appendChild(app.createElement('button', 'card--color--default', null));
   section.appendChild(app.createElement('button', 'card--color--red', null));
-  section.appendChild(app.createElement('button', 'card--color--green', null));
+  section.appendChild(app.createElement('button', 'card--color--orange', null));
   section.appendChild(app.createElement('button', 'card--color--blue', null));
   
   return section;
 },
 
 changeCardColor:() => {
-  document.querySelectorAll('.card--color--red, .card--color--green, .card--color--blue, .card--color--default').forEach(button => {
+  document.querySelectorAll('.card--color--red, .card--color--orange, .card--color--blue, .card--color--default').forEach(button => {
     button.addEventListener('click', (event) => {
       event.target.closest('div').className = '';
       event.target.closest('div').classList.add('draggable--card');
@@ -97,8 +124,10 @@ createElement:(tag, className, textContent) => {
   return element;
 },
 
-createInputElement:(inputType, className, placeHolder) => {
+createInputElement:(inputType, atribute, name, className, placeHolder) => {
   const element = document.createElement(inputType);
+  element.setAttribute('type', atribute);
+  element.setAttribute('name', name);
   element.classList.add(className);
   element.placeholder = placeHolder;
   return element;
